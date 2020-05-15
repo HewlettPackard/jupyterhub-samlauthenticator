60a2bdca006f2f7b70e2d20ae9fdc21e5f19c6e8
Tom Kelley <distortedsignal@gmail.com>
2020-05-15 09:47:03 -0700

    Increment version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ OTHER DEALINGS IN THE SOFTWARE.
 
 from setuptools import setup
 
-version = '0.0.7'
+version = '0.0.8'
 
 with open('requirements.txt', 'r') as req_file, \
         open('test_requirements.txt', 'r') as test_req_file, \
